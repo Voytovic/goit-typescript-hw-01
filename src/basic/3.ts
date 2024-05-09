07294283f65c1a94826df5a0085bab49188e27d0
Voytovic <voytovic@gmail.com>
2024-05-09 20:28:12 +0300

    1

---
+++ src/basic/3.ts
@@ -0,0 +1,10 @@
+let numberOrString: string | number;
+
+numberOrString = "Hello";
+numberOrString = 14;
+
+type enableOrDisable = "enable" | "disable";
+let check: enableOrDisable;
+
+check = "enable";
+check = "disable";
